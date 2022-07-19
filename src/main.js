8729cc7ffe7b379b3da2409a108e11d54d9a72c9
lxx0303 <2434921421@qq.com>
2022-07-19 15:20:10 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "@/styles/quick-style.less";
+import ElementUI from "element-ui";
+import Element from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
+Vue.use(Element, { size: "small", zIndex: 3000 });
 
 new Vue({
+  el: "#app",
   router,
   store,
   render: (h) => h(App),
